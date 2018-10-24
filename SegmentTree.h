8b809bd35e0cdc2e1f8728893c1cd967784cbb67
Mikhail Yurovskiy <mikhail.yurovskiy@gmail.com>
2018-10-23 18:13:22 -0600

    Initial commit

---
+++ SegmentTree.h
@@ -0,0 +1,104 @@
+#ifndef SEGMENTTREE_H
+#define SEGMENTTREE_H
+
+#include <type_traits>
+#include <iterator>
+
+namespace SegmentTree
+{
+	namespace Details
+	{
+		template <typename T>
+		T roundup2k( T value )
+		{
+			static_assert( std::is_integral<T>::value, "Integral type required." );
+			using UT = std::remove_cv_t<std::make_unsigned_t<T>>;
+			UT v = static_cast<UT>( value );
+			--v;
+			v |= v >> 1;
+			v |= v >> 2;
+			v |= v >> 4;
+			if (sizeof( v ) >= 2)
+				v |= v >> 8;
+			if (sizeof( v ) >= 4)
+				v |= v >> 16;
+			if (sizeof( v ) >= 8)
+				v |= v >> 32;
+			return ++v;
+		}
+
+
+		template <typename It, typename BinaryOp>
+		auto get_range( It from, It to, It range_begin, It range_end, It level, It node, typename std::iterator_traits<It>::difference_type range_size, BinaryOp op )
+		{
+			// Check complete match of the interval to the current range.
+			if (from == range_begin && to == range_end)
+				return *node;
+
+			// Move down to the child nodes.
+			range_size <<= 1;
+			auto const node_index = (node - level) << 1;
+			level -= range_size;
+			node   = level + node_index;
+
+			// Calculate child range.
+			auto const range_mid = range_begin + ((range_end - range_begin) >> 1);
+
+			// Check if the interval is completely owned by one of the child nodes.
+			if (from >= range_mid)
+				return get_range( from, to, range_mid, range_end, level, ++node, range_size, op );
+			if (to <= range_mid)
+				return get_range( from, to, range_begin, range_mid, level, node, range_size, op );
+
+			// General case: divide interval by the child nodes.
+			auto const left  = get_range( from, range_mid, range_begin, range_mid, level, node, range_size, op );
+			auto const right = get_range( range_mid, to, range_mid, range_end, level, ++node, range_size, op );
+			return op( left, right );
+		}
+	}  // namespace Details
+
+
+	template <typename Container, typename BinaryOp>
+	void prepare( Container& segment_tree, BinaryOp op )
+	{
+		// Round up source array length to the nearest power of two and
+		// fill it up to the rounded length with the default elements.
+		auto tree_len = Details::roundup2k( segment_tree.size() );
+		segment_tree.resize( tree_len );
+		segment_tree.reserve( tree_len << 1 );
+
+		// Fill tree levels from the leafs to the root.
+		for (auto it = segment_tree.begin(); tree_len > 1; tree_len >>= 1)
+		{
+			auto range_end = it + tree_len;
+			while (it != range_end)
+			{
+				auto& left  = *it++;
+				auto& right = *it++;
+				// Reserve at the function begin must guarantee that
+				// the iterator 'it' will remain valid.
+				segment_tree.emplace_back( op( left, right ) );
+			}
+		}
+	}
+
+
+	template <typename Container, typename BinaryOp>
+	void modify( Container& segment_tree, typename Container::iterator pos, typename Container::value_type&& value, BinaryOp op )
+	{
+		// TODO
+	}
+
+
+	template <typename Container, typename BinaryOp>
+	auto get_range( Container const& segment_tree, typename Container::const_iterator from, typename Container::const_iterator to, BinaryOp op )
+	{
+		// Begin from the root (the last element by design). The root range is the whole array.
+		auto const range_begin = segment_tree.begin();
+		auto const range_end   = range_begin + ((segment_tree.size() + 1) >> 1);
+		auto const root        = segment_tree.end() - 1;
+		return Details::get_range( from, to, range_begin, range_end, root, root, 1, op );
+	}
+}  // namespace SegmentTree
+
+#endif //SEGMENTTREE_H
---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include "SegmentTree.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	{
+		vector<int> segtree = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+		auto const original_size = segtree.size();
+		SegmentTree::prepare( segtree, plus<>() );
+
+		bool result = true;
+		for (auto i = segtree.begin(); i < segtree.begin() + original_size; ++i) for (auto j = i + 1; j <= segtree.begin() + original_size; ++j)
+		{
+			auto const range = SegmentTree::get_range( segtree, i, j, plus<>() );
+			int expected = 0;
+			for (auto k = i; k < j; ++k)
+				expected += *k;
+			if (range != expected)
+			{
+				result = false;
+				printf( "%ld..%ld = %d [%d] [FAILURE]\n", i - segtree.begin(), j - segtree.begin(), range, expected );
+			}
+		}
+		if (result)
+			printf( "SUCCESS\n" );
+	}
+
+	{
+		vector<int> segtree = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+		auto const original_size = segtree.size();
+		auto maxop = []( int a, int b ) { return max( a, b ); };
+		SegmentTree::prepare( segtree, maxop );
+
+		bool result = true;
+		for (auto i = segtree.begin(); i < segtree.begin() + original_size; ++i) for (auto j = i + 1; j <= segtree.begin() + original_size; ++j)
+		{
+			auto const range = SegmentTree::get_range( segtree, i, j, maxop );
+			int expected = 0;
+			for (auto k = i; k < j; ++k)
+				expected = max( expected, *k );
+			if (range != expected)
+			{
+				result = false;
+				printf( "%ld..%ld = %d [%d] [FAILURE]\n", i - segtree.begin(), j - segtree.begin(), range, expected );
+			}
+		}
+		if (result)
+			printf( "SUCCESS\n" );
+	}
+
+	return 0;
+}
